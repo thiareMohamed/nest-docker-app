7f8d284b8d17182dd6de3cc503f22323e43bc5da
thiareMohamed <thiaremohamed.mt@gmail.com>
2024-12-05 13:42:20 +0000

    Ajout de la configuration de la base de données PostgreSQL dans docker-compose et mise à jour des paramètres d'environnement dans app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST || 'localhost',
-      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-      username: process.env.DATABASE_USERNAME || 'myuser',
-      password: process.env.DATABASE_PASSWORD || 'mypassword',
-      database: process.env.DATABASE_NAME || 'mydb',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
